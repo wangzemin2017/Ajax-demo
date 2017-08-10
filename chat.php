15534fa50a9b5d205d5112af14608b0d628c0a61
wangzemin <1966199652@qq.com>
2017-08-10 22:03:33 +0800

    Ajax demo about 'post' request

---
+++ chat.php
@@ -0,0 +1,20 @@
+<?php
+
+
+	$messages = array(
+		'你说啥？',
+		'你也好',
+		'你找我有啥事？',
+		'我在吃饭！'
+	);
+
+
+	// 通过array_rand()随机获取数组下标
+	// var_dump(array_rand($messages));
+
+	echo $messages[array_rand($messages)];
+
+	sleep(1);
+
+
+?>
